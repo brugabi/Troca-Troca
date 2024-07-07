0b987194dfc7a89ef17ff2930a1d7d7e285e9434
Rafael Queiroz Santos <rafaelqsantos33@gmail.com>
2024-07-07 17:48:59 -0300

    modified:   spring-api/src/main/java/com/uneb/spring_api/controller/PropostaController.java 	modified:   spring-api/src/main/java/com/uneb/spring_api/dto/PropostaDTO.java

--- spring-api/src/main/java/com/uneb/spring_api/controller/PropostaController.java
+++ spring-api/src/main/java/com/uneb/spring_api/controller/PropostaController.java
@@ -1,2 +1,46 @@
-package com.uneb.spring_api.controller;public class PropostaControle {
+package com.uneb.spring_api.controller;
+
+import com.uneb.spring_api.dto.PropostaDTO;
+import com.uneb.spring_api.models.Anuncio;
+import com.uneb.spring_api.models.Proposta;
+import com.uneb.spring_api.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/proposta")
+public class PropostaController {
+
+    @Autowired
+    private PropostaController propostaController;
+
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private AnuncioController anuncioController;
+
+    @PostMapping("/criar")
+    public ResponseEntity<Map<String,Object>> createProposta(@RequestBody PropostaDTO propostaDTO) {
+        Proposta proposta = new Proposta();
+        Map<String,Object> response = new HashMap<>();
+        Optional<User> requisitante = userController.obterUsuario(propostaDTO.idRequisitante());
+        if (!requisitante.isPresent()){
+            response.put("message","Requisitante não existe no banco de usuários");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        Optional<Anuncio> anuncio = anuncioController.verAnuncio(propostaDTO.idAnuncio());
+        if ()
+    }
+
+
 }
--- spring-api/src/main/java/com/uneb/spring_api/dto/PropostaDTO.java
+++ spring-api/src/main/java/com/uneb/spring_api/dto/PropostaDTO.java
@@ -1,2 +1,12 @@
-package com.uneb.spring_api.dto;public record PropostaDTO() {
+package com.uneb.spring_api.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public record PropostaDTO(
+
+        @NotNull Long idRequisitante,
+        @NotNull Long idAnuncio,
+        @NotNull String textoProposta
+
+) {
 }

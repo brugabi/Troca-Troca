0de795485433ce8c110489d8c808ed256332ee80
Carlos Erick <73728755+Carlos0101py@users.noreply.github.com>
2024-07-07 15:22:50 -0300

    modificação do /login

--- spring-api/src/main/java/com/uneb/spring_api/controller/AuthController.java
+++ spring-api/src/main/java/com/uneb/spring_api/controller/AuthController.java
@@ -1,12 +1,6 @@
 package com.uneb.spring_api.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +14,8 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @GetMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestBody User user) {
-        Long userId = authService.authenticateAndGetUserId(user.getLogin(), user.getSenha());
-        Map<String, Object> response = new HashMap<>();
-        if (userId != null) {
-            response.put("userId", userId);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } else {
-            response.put("message", "Invalid login or password");
-            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
-        }
+    @PostMapping("/login")
+    public Long login(@RequestBody User user) {
+        return authService.authenticateAndGetUserId(user.getLogin(), user.getSenha());
     }
 }

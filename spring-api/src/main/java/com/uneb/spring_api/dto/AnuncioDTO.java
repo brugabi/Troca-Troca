aab636d5915b063bd82c61cdb79dc5730eabf506
Rafael Queiroz Santos <rafaelqsantos33@gmail.com>
2024-07-07 12:21:44 -0300

    att anuncio( Foda-se)

--- spring-api/src/main/java/com/uneb/spring_api/dto/AnuncioDTO.java
+++ spring-api/src/main/java/com/uneb/spring_api/dto/AnuncioDTO.java
@@ -4,39 +4,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public class AnuncioDTO {
-    @NotBlank(message = "O título deve ser informado.")
-    @Size(min = 2, max = 255, message = "O título deve ter entre 2 e 255 caracteres.")
-    private String titulo;
+public record AnuncioDTONew(
 
-    @NotBlank(message = "A descrição deve ser informada.")
-    @Size(min = 2, message = "A descrição deve ter no mínimo 2 caracteres.")
-    private String descricao;
+        @NotBlank(message = "O título deve ser informado.")
+        @Size(min = 2, max = 255, message = "O título deve ter entre 2 e 255 caracteres.")
+        String titulo,
 
-    @NotNull(message = "O ID do criador do anúncio deve ser informado.")
-    private Long idCriador;
+        @NotBlank(message = "A descrição deve ser informada.")
+        @Size(min = 2, message = "A descrição deve ter no mínimo 2 caracteres.")
+        String descricao,
 
-    public @NotBlank(message = "O título deve ser informado.") @Size(min = 2, max = 255, message = "O título deve ter entre 2 e 255 caracteres.") String getTitulo() {
-        return titulo;
-    }
+        @NotNull(message = "O ID do criador do anúncio deve ser informado.")
+        Long idCriador
 
-    public void setTitulo(@NotBlank(message = "O título deve ser informado.") @Size(min = 2, max = 255, message = "O título deve ter entre 2 e 255 caracteres.") String titulo) {
-        this.titulo = titulo;
-    }
-
-    public @NotBlank(message = "A descrição deve ser informada.") @Size(min = 2, message = "A descrição deve ter no mínimo 2 caracteres.") String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(@NotBlank(message = "A descrição deve ser informada.") @Size(min = 2, message = "A descrição deve ter no mínimo 2 caracteres.") String descricao) {
-        this.descricao = descricao;
-    }
-
-    public @NotNull(message = "O ID do criador do anúncio deve ser informado.") Long getIdCriador() {
-        return idCriador;
-    }
-
-    public void setIdCriador(@NotNull(message = "O ID do criador do anúncio deve ser informado.") Long idCriador) {
-        this.idCriador = idCriador;
-    }
-}
+) {}

4989a5525e7f45dd6a87541c26398695183b159b
Carlos Erick <73728755+Carlos0101py@users.noreply.github.com>
2024-07-09 23:19:09 -0300

    cabou

--- sistemadetroca-front/components/navbar.js
+++ sistemadetroca-front/components/navbar.js
@@ -3,16 +3,16 @@ import Link from 'next/link';
 
 const NavBar = () => {
     return (
-        <div className="flex items-center w-full h-[4rem] shadow-lg border-2 border-opacity-1 px-[3rem] bg-white">
+        <div className="flex items-center justify-between w-full h-[4rem] shadow-lg border-2 border-opacity-1 px-[3rem] bg-white">
             <div className="flex justify-start items-center w-[33%]">
                 <Link href="/home">
                     <Image src="/image/troca-logo.png" alt="Logo" width={120} height={64} />
                 </Link>
             </div>
-            <div className="flex border rounded-xl p-1 w-[33%]">
+            {/* <div className="flex border rounded-xl p-1 w-[33%]">
                 <input className="w-full focus:outline-none" placeholder="Pesquisar..."/>
                 <Image src="/icons/search.svg" alt="User Icon" width={25} height={25} />
-            </div>
+            </div> */}
             <div className="flex w-[33%] justify-end">
                 <nav className="flex gap-4">
                     <Link href="/home/newpost">
